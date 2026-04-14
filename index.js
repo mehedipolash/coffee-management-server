ae72eba561f701ad454c05913c41caaed692581a
mehedipolash <mehedipolash228@gmail.com>
2026-04-14 13:52:19 +0600

    signin implemeted using PATCH so user login time updated

--- index.js
+++ index.js
@@ -109,7 +109,18 @@ async function run() {
       res.send(result);
     });
 
-
+    app.patch('/users', async (req, res) => {
+      console.log(req.body);
+      const {email,lastSignInTime,}= req.body;
+      const filter= {email :email}
+      const updatedDoc ={
+         $set:{
+             lastSignInTime:lastSignInTime
+         }
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
